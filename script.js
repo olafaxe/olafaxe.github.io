0b86d13b8b3541c083468b942fa74596e69e381a
Ola Faxe <olafaxe@gmail.com>
2019-06-18 19:14:08 +0200

    Responsive fixes and added a clock

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("hej då");
-console.log("test commit");
+let klocka = document.querySelector("#klocka");
+
+setInterval(
+  (clock = () => {
+    let todayDate = new Date();
+
+    let hours = todayDate.getHours();
+    let minutes = todayDate.getMinutes();
+    let seconds = todayDate.getSeconds();
+    if (seconds < 10) {
+      seconds = `0${todayDate.getSeconds()}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${todayDate.getMinutes()}`;
+    }
+    if (hours < 10) {
+      hours = `0${todayDate.getHours()}`;
+    }
+    let time = `${hours}:${minutes}:${seconds}`;
+
+    klocka.innerHTML = time;
+  }),
+  500
+);
