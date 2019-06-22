cdd84661812081fead95499494e0b2cad35607a9
Ola Faxe <olafaxe@gmail.com>
2019-06-22 22:55:12 +0200

    bigger grid, small animations, hit/miss click js

--- gridlock/gridlock.js
+++ gridlock/gridlock.js
@@ -6,8 +6,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   gridBox.addEventListener("mousedown", event => {
     let targetBox = "box1";
+    let clickBox = event.target.id;
+    let boxSelect = document.querySelector("#" + clickBox);
     if (event.target.id == targetBox) {
       box1.classList.add("locked");
+      box1.classList.remove("target");
+    } else {
+      boxSelect.classList.add("miss");
     }
     console.log("clicked in grid", event.target.id);
   });
