7bd8790964086c3903efcb94850b5a1eb9b3a5be
Ola Faxe <olafaxe@gmail.com>
2019-06-23 16:03:17 +0200

    added timer, working on click-status

--- gridlock/gridlock.js
+++ gridlock/gridlock.js
@@ -7,6 +7,7 @@ let startButton = document.querySelector(".startbutton-button");
 const target = { target: "" };
 
 gridBox.addEventListener("mousedown", event => {
+  let tempNum = getRandomInt(3000);
   console.log(target["target"]);
   let clickBox = event.target.id;
   let boxSelect = document.querySelector("#" + clickBox);
@@ -14,17 +15,26 @@ gridBox.addEventListener("mousedown", event => {
   if (event.target.id == target["target"]) {
     boxSelect.classList.add("locked");
     boxSelect.classList.remove("target");
+    setTimeout(startGame, tempNum);
   } else {
     boxSelect.classList.add("miss");
     targetBox.classList.remove("target");
+    setTimeout(startGame, tempNum);
   }
   console.log("clicked in grid", event.target.id);
 });
-startButton.addEventListener("click", event => {
-  startGame();
-});
+// startButton.addEventListener("click", event => {
+//   startGame();
+// });
 function startGame() {
+  let tempArr = [];
   let tempNum = getRandomInt(15);
+  for (let i = 0; i < tempArr.length; i++) {
+    if (tempArr[i] === tempNum) {
+      console.log("duplicate");
+    }
+  }
+  tempArr.push(tempNum);
   let targetBox = "box" + tempNum;
   let boxSelect = document.querySelector("#" + targetBox);
   boxSelect.classList.add("target");
@@ -35,3 +45,19 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max) + 1);
 }
 // });
+
+startButton.addEventListener("click", event => {
+  let num = 3;
+  let countDown = document.querySelector(".countdown");
+  countDown.innerHTML = num;
+  let countDownTimer = setInterval(() => {
+    num--;
+    countDown.innerHTML = num;
+
+    if (num === 0) {
+      clearInterval(countDownTimer);
+      countDown.innerHTML = "";
+    }
+  }, 1000);
+  setTimeout(startGame, 3000);
+});
