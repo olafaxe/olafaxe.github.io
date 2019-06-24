d1f51d9ba30d63ba6f09d1e5e88cfb881a4bbb39
Ola Faxe <olafaxe@gmail.com>
2019-06-24 14:25:43 +0200

    added debounce and fixed some target/miss bugs

--- gridlock/gridlock.js
+++ gridlock/gridlock.js
@@ -5,59 +5,224 @@
 let gridBox = document.querySelector(".container");
 let startButton = document.querySelector(".startbutton-button");
 const target = { target: "" };
+let tempArr = [];
+let gameProgress = false;
+let endGame = false;
+let remove = false;
+let strikes = 0;
 
-gridBox.addEventListener("mousedown", event => {
+let actionClickDebounce = debounce(event => {
+  if (event.target.id === "" || !gameProgress) {
+    return;
+  }
+  console.log(event.target.id);
   let tempNum = getRandomInt(3000);
   console.log(target["target"]);
   let clickBox = event.target.id;
   let boxSelect = document.querySelector("#" + clickBox);
   let targetBox = document.querySelector("#" + target["target"]);
+  let countDown = document.querySelector(".countdown");
+  if (strikes == 3) {
+    gameProgress = false;
+    countDown.innerHTML = "GAME OVER!";
+    boxSelect.classList.remove("target");
+    reset();
+    return;
+  }
+
+  if (tempArr.length === 16) {
+    countDown.innerHTML = "GRIDLOCKED!";
+    gameProgress = false;
+    boxSelect.classList.add("locked");
+    boxSelect.classList.remove("target");
+
+    return;
+  }
+
   if (event.target.id == target["target"]) {
     boxSelect.classList.add("locked");
     boxSelect.classList.remove("target");
     setTimeout(startGame, tempNum);
   } else {
+    strikes++;
+    remove = true;
     boxSelect.classList.add("miss");
+    setTimeout(() => {
+      boxSelect.classList.remove("miss");
+    }, 2000);
     targetBox.classList.remove("target");
     setTimeout(startGame, tempNum);
   }
   console.log("clicked in grid", event.target.id);
+}, 250);
+
+gridBox.addEventListener("mousedown", actionClickDebounce);
+gridBox.addEventListener("mousedown", event => {
+  console.log(event.target.id);
 });
-// startButton.addEventListener("click", event => {
-//   startGame();
+
+// gridBox.addEventListener("mousedown", event => {
+//   if (event.target.id === "" || !gameProgress) {
+//     return;
+//   }
+//   console.log(event.target.id);
+//   let tempNum = getRandomInt(3000);
+//   console.log(target["target"]);
+//   let clickBox = event.target.id;
+//   let boxSelect = document.querySelector("#" + clickBox);
+//   let targetBox = document.querySelector("#" + target["target"]);
+//   let countDown = document.querySelector(".countdown");
+//     if (strikes == 3) {
+//       gameProgress = false;
+//       countDown.innerHTML = "GAME OVER!";
+//       boxSelect.classList.remove("target");
+//       reset();
+//       return;
+//     }
+
+//       if (tempArr.length === 16) {
+//         countDown.innerHTML = "GRIDLOCKED!";
+//         gameProgress = false;
+//         boxSelect.classList.add("locked");
+//         boxSelect.classList.remove("target");
+
+//         return;
+//       }
+
+//         if (event.target.id == target["target"]) {
+//           boxSelect.classList.add("locked");
+//           boxSelect.classList.remove("target");
+//           setTimeout(startGame, tempNum);
+//         } else {
+//           strikes++;
+//           remove = true;
+//           boxSelect.classList.add("miss");
+//           setTimeout(() => {
+//             boxSelect.classList.remove("miss");
+//           }, 2000);
+//           targetBox.classList.remove("target");
+//           setTimeout(startGame, tempNum);
+//         }
+//   console.log("clicked in grid", event.target.id);
 // });
+
+////////////////////////7
+
 function startGame() {
-  let tempArr = [];
-  let tempNum = getRandomInt(15);
-  for (let i = 0; i < tempArr.length; i++) {
-    if (tempArr[i] === tempNum) {
-      console.log("duplicate");
+  if (remove) {
+    tempArr.pop();
+    remove = false;
+  }
+  let tempNum = randomIntFromInterval(1, 16);
+  function randomNummerLoop() {
+    for (let i = 0; i < tempArr.length; i++) {
+      if (tempArr[i] === tempNum || tempNum === 0) {
+        tempNum = randomIntFromInterval(i, 16);
+        randomNummerLoop();
+      }
     }
   }
+  randomNummerLoop();
   tempArr.push(tempNum);
   let targetBox = "box" + tempNum;
   let boxSelect = document.querySelector("#" + targetBox);
+  // if (boxSelect.classList.contains("locked")) {
+  //   remove = true;
+  //   startGame();
+  // }
+  boxSelect.classList.remove("miss");
   boxSelect.classList.add("target");
   target["target"] = targetBox;
 }
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max) + 1);
+  return Math.floor(Math.random() * Math.floor(max));
 }
 // });
 
-startButton.addEventListener("click", event => {
-  let num = 3;
-  let countDown = document.querySelector(".countdown");
-  countDown.innerHTML = num;
-  let countDownTimer = setInterval(() => {
-    num--;
-    countDown.innerHTML = num;
+function randomIntFromInterval(min, max) {
+  // min and max included (stackoverflow)
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-    if (num === 0) {
-      clearInterval(countDownTimer);
-      countDown.innerHTML = "";
+startButton.addEventListener("click", event => {
+  if (!gameProgress) {
+    reset();
+    for (let i = 1; i < tempArr.length + 1; i++) {
+      let targetBox = "#box" + i;
+      let boxSelect = document.querySelector(targetBox);
+      boxSelect.classList.remove("locked");
+      boxSelect.classList.remove("target");
+      boxSelect.classList.remove("miss");
     }
-  }, 1000);
-  setTimeout(startGame, 3000);
+    tempArr = [];
+    let num = 3;
+    let countDown = document.querySelector(".countdown");
+    countDown.innerHTML = num;
+    let countDownTimer = setInterval(() => {
+      num--;
+      countDown.innerHTML = num;
+
+      if (num === 0) {
+        clearInterval(countDownTimer);
+        countDown.innerHTML = "";
+      }
+    }, 1000);
+    setTimeout(startGame, 3000);
+  }
+  gameProgress = true;
+
+  // for (let i = 1; i < tempArr.length + 1; i++) {
+  //   let targetBox = "#box" + i;
+  //   let boxSelect = document.querySelector(targetBox);
+  //   boxSelect.classList.remove("locked");
+  // }
+  // tempArr = [];
+  // let num = 3;
+  // let countDown = document.querySelector(".countdown");
+  // countDown.innerHTML = num;
+  // let countDownTimer = setInterval(() => {
+  //   num--;
+  //   countDown.innerHTML = num;
+
+  //   if (num === 0) {
+  //     clearInterval(countDownTimer);
+  //     countDown.innerHTML = "";
+  //   }
+  // }, 1000);
+  // setTimeout(startGame, 3000);
 });
+
+function reset() {
+  for (let i = 0; i < tempArr.length; i++) {
+    let k = tempArr[i];
+    let targetBox = document.querySelector("#box" + k);
+    targetBox.classList.remove("locked");
+    targetBox.classList.remove("target");
+    targetBox.classList.remove("miss");
+  }
+  endGame = false;
+  strikes = 0;
+}
+
+function debounce(func, wait, immediate) {
+  var timeout;
+
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, args);
+  };
+}
